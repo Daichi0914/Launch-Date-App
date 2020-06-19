58255afefa65ed00f24b16ab3eebb0a494f188e7
Daichi0914 <goludrozya44@gmail.com>
2020-06-20 01:39:20 +0900

    Gridの部分の編集。mapを使えるように。

--- src/CONTENTS/TOP/TOP_CONTENTS/TrendList.jsx
+++ src/CONTENTS/TOP/TOP_CONTENTS/TrendList.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import TrendCard from '../TOP_LIST/TrendCard';
 
+import classes from './list.module.css';
+
 const TrendList = () => {
   return (
-    <div style={{
-      height: '100%',
-      padding: '25px 25px 0 280px',
-    }}>
-      <Grid columns={5} divided>
+    <div className={classes.list}>
+      <Grid columns={4}>
         {/* 以下mapを使う */}
         <Grid.Row>
           <Grid.Column>
@@ -26,15 +25,33 @@ const TrendList = () => {
           <Grid.Column>
             <TrendCard />
           </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
           <Grid.Column>
             <TrendCard />
           </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
+          <Grid.Column>
+            <TrendCard />
+          </Grid.Column>
           <Grid.Column>
             <TrendCard />
           </Grid.Column>
--- src/CONTENTS/TOP/TOP_LIST/TrendCard.jsx
+++ src/CONTENTS/TOP/TOP_LIST/TrendCard.jsx
@@ -5,11 +5,12 @@ import faker from 'faker';
 import 'semantic-ui-css/semantic.min.css';
 
 const TrendList = () => (
-  <Card>
+  <Card style={{marginTop: '20px'}}>
     <Image src={faker.image.avatar()} wrapped ui={false} />
     <Card.Content>
       <Card.Header>作品名</Card.Header>
-      <Card.Meta>＊巻</Card.Meta>
+      <Card.Meta>作者</Card.Meta>
+      {/* <Card.Meta>＊巻</Card.Meta> */}
       <Card.Meta>出版社</Card.Meta>
     </Card.Content>
     <Card.Content extra>
