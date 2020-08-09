6d947a688033233a62cc2e05c3e4a89b96470e2c
Daichi0914 <goludrozya44@gmail.com>
2020-08-09 22:12:31 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Launch-Date-App/precache-manifest.789dd87be7a8071583c779b4f46345fb.js"
+  "/Launch-Date-App/precache-manifest.ba050e746ea0558b2cd806301b18a155.js"
 );
 
 self.addEventListener('message', (event) => {
